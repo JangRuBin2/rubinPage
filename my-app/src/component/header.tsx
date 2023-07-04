878af9af686dff36fced8077b1801390ad5604ce
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-04 23:46:02 +0900

    리액트 개발자 도구 사용

--- my-app/src/component/header.tsx
+++ my-app/src/component/header.tsx
@@ -1,27 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import React from 'react';
 
-// 인터페이스
 type HeaderProps = {
   title: string;
 };
-interface TitleMapping {
-  [key: string]: string;
-}
-// path에 따른 문자 데이터
-const titleMapping: TitleMapping = {
-  '/': '홈',
-};
-export const Header: React.FC<HeaderProps> = ({ title }) => {
-  const location = useLocation();
-  const [pageTitle, setPageTitle] = useState(title);
-    useEffect(() => {
-    setPageTitle(titleMapping[location.pathname] || '');
-  }, [location.pathname]);
+
+const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
     <header>
-      <h1>{pageTitle}</h1>
+      <h1>{title}</h1>
     </header>
   );
 };
 
+export default Header;
