299ea9f1463366183e7dc8bd9fa3a340b4967362
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-04 23:39:52 +0900

    헤더 컴포넌트 제작

--- my-app/src/component/header.tsx
+++ my-app/src/component/header.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
+// 인터페이스
 type HeaderProps = {
   title: string;
 };
-
-const Header: React.FC<HeaderProps> = ({ title }) => {
+interface TitleMapping {
+  [key: string]: string;
+}
+// path에 따른 문자 데이터
+const titleMapping: TitleMapping = {
+  '/': '홈',
+};
+export const Header: React.FC<HeaderProps> = ({ title }) => {
+  const location = useLocation();
+  const [pageTitle, setPageTitle] = useState(title);
+    useEffect(() => {
+    setPageTitle(titleMapping[location.pathname] || '');
+  }, [location.pathname]);
   return (
     <header>
-      <h1>{title}</h1>
+      <h1>{pageTitle}</h1>
     </header>
   );
 };
 
-export default Header;
