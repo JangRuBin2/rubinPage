3782562dec544d64426e17e645acc34bf1fbed91
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-08 17:49:33 +0900

    페이지 넘길 때 이벤트->fade-in 애니메이션 추가

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -11,20 +11,28 @@ function App() {
   const pages = [<IntroPage />, <AboutMePage />, <PorfolPage />, <FunnyPage />];
   // 페이지 상태 관리
   const [currentIndex, setCurrentIndex] = useState(0);
+  // 애니메이션 상태 관리
+  const [animationClassName, setAnimationClassName] = useState("fade-in");
   // 페이지 index 갱신 함수
   const changPage = (direction : string)=> {
+    // 애니메이션 클래스 제거
+    setAnimationClassName("");
     if (direction === 'next') {
       setCurrentIndex((currentIndex + 1)% pages.length);
     } else if (direction === 'prev'){
       setCurrentIndex((currentIndex - 1 + pages.length)%pages.length)
     }
+
+    setTimeout(() => {
+      setAnimationClassName("fade-in"); // 애니메이션 클래스를 다시 적용합니다.
+    }, 50);
   }
   return (
     <>
       <div onClick={() => changPage("prev")}>
         <UpBtn />
       </div>
-      <div>{pages[currentIndex]}</div>
+      <div className={animationClassName}>{pages[currentIndex]}</div>
       <div onClick={() => changPage("next")}>
         <DownBtn />
       </div>
