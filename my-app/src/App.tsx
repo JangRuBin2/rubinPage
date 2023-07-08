7ef8e8a9537b1dd1e2b185c2b9780eaefcb9dbd6
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-08 16:04:00 +0900

    페이지 공용 CSS 적용 및 intro페이지 CSS 적용

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,6 +3,8 @@ import AboutMePage from "./pages/aboutMePage";
 import FunnyPage from "./pages/funnyPage";
 import IntroPage from "./pages/introPage";
 import PorfolPage from "./pages/portfolioPage";
+// 각 페이지 공용 css
+import './style/commonPageSty.css';
 function App() {
   return (
     <>
--- my-app/src/service/intro.tsx
+++ my-app/src/service/intro.tsx
@@ -1,6 +1,12 @@
 import FlySqr from "../images/me.png";
-import '../style/commonPageSty.css';
+import '../style/introPage.css';
 const Intro = ():JSX.Element => {
-  return (<div> <img src={FlySqr} alt="이미지 설명" style={{width : '200px', height : '200px'}}/></div>)
+  return (
+  <div className="container">
+    <div className="introBox">
+      <img src={FlySqr} alt="이미지 설명" style={{width : '200px', height : '200px'}}/>
+        <h3>루빈</h3>
+    </div>
+  </div>)
 }
 export default Intro;
