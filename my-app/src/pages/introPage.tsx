f7a7bd735c8e1a932fe9692ee34932426b24ceb2
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-08 15:30:44 +0900

    페이지 공용 css 제작, 적용 확인

--- my-app/src/pages/introPage.tsx
+++ my-app/src/pages/introPage.tsx
@@ -1,7 +1,8 @@
 import Intro from "../service/intro";
+import "../style/commonPageSty.css";
 const IntroPage = ():JSX.Element => {
   
-  return (<div>인트로 페이지<Intro /></div>
+  return (<div  className="wrap">인트로 페이지<Intro /></div>
   )
 }
 export default IntroPage;
