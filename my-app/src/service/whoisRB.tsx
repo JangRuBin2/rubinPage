8f60828f2e0ef42d52a33838805e9f4b5fa5b544
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-09 09:22:38 +0900

    클릭하면 새로운 div 생기는 이벤트 추가, (내 소개)

--- my-app/src/service/whoisRB.tsx
+++ my-app/src/service/whoisRB.tsx
@@ -1,5 +1,10 @@
+import { useState } from 'react';
 import UmRB from "../images/umRubin.webp";
 const AboutMe = () : JSX.Element=> {
+  const [showNewDiv, setShowNewDiv] = useState(false);
+  const handleShowNewDiv = () => {
+    setShowNewDiv(!showNewDiv);
+};
   return (
       <div className="container">
         <div style={{display:"flex",flexDirection:"column", justifyContent : "space-around", alignItems:"center"}}>
@@ -8,6 +13,8 @@ const AboutMe = () : JSX.Element=> {
           </div>
           <div>
           <h2>안녕하세요. 튼튼한 개발자 장루빈입니다!</h2>
+          <div  onClick={handleShowNewDiv}>눌러줘 나를</div>
+          {showNewDiv && <div className="new-div">새로운 div</div>}
           </div>
         </div>
       </div>
