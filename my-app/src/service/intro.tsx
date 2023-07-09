9e2431471dfec822fe139b6c6cb153c07b8d5fef
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-09 19:37:35 +0900

    유튜브 영상 불러오는 것 확인

--- my-app/src/service/intro.tsx
+++ my-app/src/service/intro.tsx
@@ -1,12 +1,22 @@
+import YouTube from 'react-youtube';
 import FlySqr from "../images/me.png";
 import '../style/introPage.css';
 const Intro = ():JSX.Element => {
+  const videoId = 'OzmF8WZ5QgQ';
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
   return (
   <div className="container">
     <div className="introBox">
       <img src={FlySqr} alt="이미지 설명" style={{width : '200px', height : '200px'}}/>
         <h2>RuBin</h2>
         <h4>fullstack developer</h4>
+        <YouTube videoId={videoId} opts={opts} />
     </div>
   </div>)
 }
