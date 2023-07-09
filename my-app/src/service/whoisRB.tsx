bc59a3ade1f4ad0d09f052a9683b33b7b0b18536
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-09 10:36:55 +0900

    토글 이벤트

--- my-app/src/service/whoisRB.tsx
+++ my-app/src/service/whoisRB.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import UmRB from "../images/umRubin.webp";
 import '../style/aboutMe.css';
+import { ToggleOff, ToggleOn } from './toggle';
 const AboutMe = () : JSX.Element=> {
   const [showNewDiv, setShowNewDiv] = useState(false);
   const handleShowNewDiv = () => {
@@ -14,7 +15,7 @@ const AboutMe = () : JSX.Element=> {
             <h2>flying squirrel</h2>
           </div>
           
-          <div  onClick={handleShowNewDiv}>눌러줘 나를</div>
+          <div  onClick={handleShowNewDiv}> {!showNewDiv ? <ToggleOff /> : <ToggleOn />}</div>
           {showNewDiv && <h4>저는 새로운 것에 도전하는 것을 즐깁니다.<br/>
             언어와 도구에 제약 받지 않고, 새로운 것에 거부감 없이 도전합니다.<br/>
             제한 없이 다양한 환경에서 발전하는 개발자가 되겠습니다.
