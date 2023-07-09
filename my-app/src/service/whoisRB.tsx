b5967fa811fc6b847359eaada305c447e24d8e19
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-09 09:09:50 +0900

    내 소개 페이지, 컴포넌트 제작

--- my-app/src/service/whoisRB.tsx
+++ my-app/src/service/whoisRB.tsx
@@ -1,9 +1,16 @@
 import UmRB from "../images/umRubin.webp";
 const AboutMe = () : JSX.Element=> {
   return (
-    
-      <img src={UmRB} alt=""/>
-      
+      <div className="container">
+        <div style={{display:"flex",flexDirection:"column", justifyContent : "space-around", alignItems:"center"}}>
+          <div style={{display :"flex", alignItems:"center",justifyContent:"center"}}>
+            <img src={UmRB} alt="" style={{width : '310px', height : '350px'}}/>
+          </div>
+          <div>
+          <h2>안녕하세요? 튼튼한 개발자 장루빈입니다!</h2>
+          </div>
+        </div>
+      </div>
   )
 }
 export default AboutMe;
