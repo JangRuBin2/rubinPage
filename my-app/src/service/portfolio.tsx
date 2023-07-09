939cfa991a46bc37dc1becb24ff1e261d04dec1b
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-09 19:28:00 +0900

    div  C, A, B순으로 변경

--- my-app/src/service/portfolio.tsx
+++ my-app/src/service/portfolio.tsx
@@ -3,10 +3,13 @@ import PrjA from "./projectA";
 import PrjB from "./projectB";
 import PrjC from "./projectC";
 const Portfolio = ():JSX.Element => {
+  // div 리스트
   const divs = [<PrjC />,<PrjA />, <PrjB />];
+  // div index 상태 관리
   const [divIndex, setDivIndex] = useState(0);
+  // 클릭 이벤트로 div 변경
   const changeDiv =()=> {
-    setDivIndex((divIndex - 1 + divs.length)%divs.length)
+    setDivIndex((divIndex + 1)% divs.length);
   }
   return (<div className="container" onClick={changeDiv}>
     {divs[divIndex]}
