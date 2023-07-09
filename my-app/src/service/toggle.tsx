d62dd7e84196ad9cf584d195940608b2830576bc
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-09 10:33:34 +0900

    토글 컴포넌트 생성

---
+++ my-app/src/service/toggle.tsx
@@ -0,0 +1,18 @@
+import toggleOff from "../images/toggle_off.png";
+import toggleOn from "../images/toggle_on.png";
+
+export const ToggleOn = () : JSX.Element=> {
+  return (
+    <>
+    <img src={toggleOn} alt="" />
+    </>
+  )
+};
+
+export const ToggleOff = () : JSX.Element=> {
+  return (
+    <>
+    <img src={toggleOff} alt="" />
+    </>
+  )
+};
