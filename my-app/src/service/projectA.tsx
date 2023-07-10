65ec6e321ba6f594c53532bf03c2dec59bac1813
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-10 22:35:39 +0900

    필요 없는 파일 제거

--- my-app/src/service/projectA.tsx
+++
@@ -1,6 +0,0 @@
-const PrjA = () : JSX.Element => {
-  return (
-    <div>프로젝트 A 내용</div>
-  )
-};
-export default PrjA;
--- my-app/src/service/projectB.tsx
+++
@@ -1,6 +0,0 @@
-const PrjB = () : JSX.Element => {
-  return (
-    <div>프로젝트 B 내용</div>
-  )
-};
-export default PrjB;
