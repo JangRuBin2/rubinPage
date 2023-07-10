8399e6b893d4ddd81ad2f8026d4c40859c5542c2
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-10 17:39:17 +0900

    프로젝트 class로 요소 생성 -> 애매하지만 일단 진행

--- my-app/src/service/stockTogether.tsx
+++ my-app/src/service/stockTogether.tsx
@@ -1,8 +1,10 @@
 import { ReactElement, ReactNode } from 'react';
 import CloseBtn from "../images/closeBtn.png";
+import Github from '../images/github.png';
+import Notion from "../images/notion.png";
 import Stock from '../images/stockT.png';
+import '../style/modal.css';
 import PortfolioContent from './modalClass';
-
 interface StockTogetherModalProps {
   handleClose: () => void;
   children?: ReactNode;
@@ -11,7 +13,9 @@ interface StockTogetherModalProps {
 const portfolioContent = new PortfolioContent(
   'https://github.com/JangRuBin2/stockTogether',
   'https://www.notion.so/kongukjae/Project-A-5-Hz-34c518401ee64f1093650ab5525aba16',
-  '주식 거래 애플리케이션입니다.',
+  `주식 거래 애플리케이션입니다.
+  주식 초보자도 쉽게 접할 수 있게, 함께하는 주식 서비스를 만들었습니다.
+  `,
   Stock
 );
 
@@ -23,18 +27,24 @@ const StockTogetherModal = ({ handleClose }: StockTogetherModalProps): ReactElem
   return (
     <>
       <div className="portfolioModal">
-        <div className='modalBox'>
-          <h1>stockTogether</h1>
+        <div className='modaltitle'>
           <img src={portfolioContent.imgUrl} alt="프로젝트 이미지" />
         </div>
         <div className='modalContent'>
+          <h1>stockTogether</h1>
           <h3>{portfolioContent.prjContent}</h3>
-          <div onClick={() => openLinkInNewTab(portfolioContent.githubLink)}>깃허브 링크</div>
-          <div onClick={() => openLinkInNewTab(portfolioContent.notionLink)}>노션 링크</div>
+          <div className='linkList'>
+          <span onClick={() => openLinkInNewTab(portfolioContent.githubLink)}>
+            <img src={Github} alt="" />
+          </span>
+          <span onClick={() => openLinkInNewTab(portfolioContent.notionLink)}>
+            <img src={Notion} alt="" />
+          </span>
+          </div>
         </div>
-        <button onClick={handleClose}>
+        <div onClick={handleClose}>
           <img src={CloseBtn} alt="포폴 모달" className='close-btn'/>
-        </button>
+        </div>
       </div>
     </>
   )
