a1587bbfb4f70a5879addae1efb8910c57c992bd
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-10 18:22:24 +0900

    최종 확인 완료, 포토폴리오 페이지 모바일css만 적용하면 끝

--- my-app/src/service/portfolio.tsx
+++ my-app/src/service/portfolio.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
+// 사진
 import PokeDigi from "../images/pokeDigi.png";
+import SmooDImg from '../images/smooD.png';
+import Stock from "../images/stockT.png";
+// 모듈
 import "../style/portfolio.css";
 import PokedigiModal from './pokedigiModal';
 import SmooD from './smooD';
 import StockTogether from './stockTogether';
-
 const Portfolio = ():JSX.Element => {
   const modalList = [
     PokedigiModal, StockTogether, SmooD // 예시용으로 세 개의 모달 컴포넌트를 modalList에 저장
@@ -23,17 +26,16 @@ const Portfolio = ():JSX.Element => {
   }
   // 포토폴리오 클래스 생성
   const portfolioList = [new PorFol("포케x디지",PokeDigi, 'C'),
-  new PorFol("stockTogether",PokeDigi, 'A'),
-  new PorFol("smooD",PokeDigi, 'B')];
+  new PorFol("stockTogether",Stock, 'A'),
+  new PorFol("smooD",SmooDImg, 'B')];
 
   return (
     <div className="container">
       <div className='portfolioBox'>
         {portfolioList.map((data, index)=> {
           const Modal = modalList[index]; // modalList에서 현재 포트폴리오 항목과 맞는 모달 컴포넌트를 선택
-          return <div key={index} className={'portfolio-item'}>
-            <img src={data.imageUrl} alt={`${index}포토폴리오`} onClick={() => handleModal(index)} style={{width: '100%',
-            height: 'auto'}}/>
+          return <div key={index} className={'portfolio-item'} onClick={() => handleModal(index)}>
+            <img src={data.imageUrl} alt={`${index}포토폴리오`}  />
             <h1>{`${data.name}`}</h1>
             {modal && activeModal === index && <Modal handleClose={() => handleModal(index)} />}
           </div>
