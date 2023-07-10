1d7951a320f2c54603ee86b5c0e13ea1170159d9
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-10 17:01:46 +0900

    정상적으로 요소 불러와지는 것 확인

--- my-app/src/service/modalClass.ts
+++ my-app/src/service/modalClass.ts
@@ -1,4 +1,4 @@
-class PortfolioContent {
+class ModalInformation {
   constructor(public githubLink : string, public notionLink : string, public prjContent : string, public imgUrl : string) {}
 }
-export default PortfolioContent;
+export default ModalInformation;
--- my-app/src/service/stockTogether.tsx
+++ my-app/src/service/stockTogether.tsx
@@ -1,37 +1,43 @@
 import { ReactElement, ReactNode } from 'react';
 import CloseBtn from "../images/closeBtn.png";
-import '../style/modal.css';
-import ModalClass from './modalClass';
+import Stock from '../images/stockT.png';
+import PortfolioContent from './modalClass';
 
 interface StockTogetherModalProps {
   handleClose: () => void;
   children?: ReactNode;
 }
-const portfolioContent = ModalClass.constructor('https://github.com/JangRuBin2/stockTogether','https://www.notion.so/kongukjae/Project-A-5-Hz-34c518401ee64f1093650ab5525aba16', '주식 거래 애플리케이션 입니다.', CloseBtn)
 
+const portfolioContent = new PortfolioContent(
+  'https://github.com/JangRuBin2/stockTogether',
+  'https://www.notion.so/kongukjae/Project-A-5-Hz-34c518401ee64f1093650ab5525aba16',
+  '주식 거래 애플리케이션입니다.',
+  Stock
+);
 
 const StockTogetherModal = ({ handleClose }: StockTogetherModalProps): ReactElement => {
- const openLinkInNewTab = (url: string) => {
+  const openLinkInNewTab = (url: string) => {
     window.open(url, '_blank');
   };
 
   return (
     <>
-    <div className="portfolioModal">
-      <div className='modalBox'>
-        <h1>stockTogether</h1>
-        <img src={portfolioContent.imgUrl} alt="프로젝트 이미지" />
-      </div>
-      <div className='modalContent'>
-      <h3>{portfolioContent.prjContent}</h3>
-      <div onClick={() => openLinkInNewTab(portfolioContent.githubLink)}>깃허브 링크</div>
-      <div onClick={() => openLinkInNewTab(portfolioContent.notionLink)}>노션 링크</div>
-      </div>
-      <button onClick={handleClose}>
-      <img src={CloseBtn} alt="포폴 모달" className='close-btn'/>
-      </button>
+      <div className="portfolioModal">
+        <div className='modalBox'>
+          <h1>stockTogether</h1>
+          <img src={portfolioContent.imgUrl} alt="프로젝트 이미지" />
+        </div>
+        <div className='modalContent'>
+          <h3>{portfolioContent.prjContent}</h3>
+          <div onClick={() => openLinkInNewTab(portfolioContent.githubLink)}>깃허브 링크</div>
+          <div onClick={() => openLinkInNewTab(portfolioContent.notionLink)}>노션 링크</div>
+        </div>
+        <button onClick={handleClose}>
+          <img src={CloseBtn} alt="포폴 모달" className='close-btn'/>
+        </button>
       </div>
     </>
   )
 };
+
 export default StockTogetherModal;
