f629b08bd4d09f239545e3a084abf03c81bca09d
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-07-10 11:35:34 +0900

    토글 이벤트 변경 및 주석에 설명 추가

--- my-app/src/service/whoisRB.tsx
+++ my-app/src/service/whoisRB.tsx
@@ -3,26 +3,25 @@ import UmRB from "../images/umRubin.webp";
 import '../style/aboutMe.css';
 import Toggle from './toggle2';
 const AboutMe = () : JSX.Element=> {
+  // div 상태 관리
   const [showNewDiv, setShowNewDiv] = useState(false);
   const handleShowNewDiv = () => {
     setShowNewDiv(!showNewDiv);
-};
+  };
   return (
       <div className="container">
         <div className='meBox'>
           <div className='meTitle'>
             <img src={UmRB} alt="" style={{width : '300px', height : '400px'}}/>
             <h1>flying squirrel</h1>
-            {/* <div onClick={handleShowNewDiv}> {!showNewDiv ? <ToggleOff /> : <ToggleOn />}</div> */}
-            <div onClick={handleShowNewDiv}>
-            <Toggle />
+            <div>
+            <Toggle handleShowNewDiv={handleShowNewDiv} />
             </div>
           </div>
           
           
-          {showNewDiv && <h3 className="flyInDiv">저는 새로운 것에 도전하는 것을 즐깁니다.<br/>
-            언어와 도구에 제약 받지 않고, 거부감 없이 도전합니다.<br/>
-            항상 발전하는 개발자가 되겠습니다.
+          {showNewDiv  && <h3 className="flyInDiv">저는 새로운 도전 도전을 즐깁니다.<br/>
+            언어와 도구에 제약 받지 않는 개발자가 되는 것이 목표입니다.<br/>
             </h3>}
         </div>
       </div>
